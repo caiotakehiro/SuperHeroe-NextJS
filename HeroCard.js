5674a9e5213fe42874a61b099c231d3551351531
caiotakehiro <144562821+caiotakehiro@users.noreply.github.com>
2024-11-19 13:09:10 -0300

    Update HeroCard.js

--- HeroCard.js
+++ HeroCard.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const HeroCard = ({ name, intelligence, strength, image }) => (
+  <article>
+    <img src={image} alt={name} />
+    <h1>{name}</h1>
+    <p>intelligence: <span style={{ width: `${intelligence}%`, backgroundColor: '#F9B32F' }}></span></p>
+    <p>strength: <span style={{ width: `${strength}%`, backgroundColor: '#FF7C6C' }}></span></p>
+  </article>
+);
+
+export default HeroCard;
